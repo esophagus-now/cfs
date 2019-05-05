bbef7fa3cb614a7a7ad12d68eeb0cabce63a6604
Marco Merlini <mahkoe@gmail.com>
2019-05-05 11:13:36 -0400

    Figured out my data structures

--- main.cpp
+++ main.cpp
@@ -1,7 +1,8 @@
 #include <iostream>
 #include <vector>
 #include <string>
-#include <cstdio>
+#include <cstdio> //For sscanf
+#include <utility> //For std::move
 
 using namespace std;
 
@@ -17,7 +18,7 @@ using namespace std;
 
 // Operations:
 // c A B 		- Copy from location B to location A (a "mov" instruction)
-//				- Example: Cm5555r3 means copy the value in r3 to memory address 5555
+//				- Example: cm5555r3 means copy the value in r3 to memory address 5555
 
 // b N A B C	- Perform binary operation N, for A = B N C
 //				- Example: b-r4r4m55 means r4 = r4 - m[55]
@@ -29,7 +30,7 @@ using namespace std;
 	
 // Note: the operations set the typical flags based on the result
 
-// j[znp] NN	- if the (selected) flag is set, jump by NN instructions (cane be negative).
+// j[znpu] NN	- if the (selected) flag is set, jump by NN instructions (can be negative). Use "u" for "uncoditional"
 
 // s N			- Perform "system call" number N
 //				- 0: "Set an alarm": process becomes runnable after r1 ticks elapse
@@ -37,6 +38,52 @@ using namespace std;
 //				- 2: "Write file": ditto
 //				- 3: "Test-and-set": process tests if mutex number r1 is zero, and atomically sets it if that is the case.
 										//otherwise, the process will block until the mutex "frees up"
+#define OPS X(CPY), X(BOP), X(UOP), X(JZ), X(JN), X(JP), X(JU), X(SYSCALL)
+typedef enum {
+#define X(x) x
+	OPS
+#undef X
+} op_t;
+char const *op_names[] = {
+#define X(x) #x
+	OPS
+#undef X
+};
+
+#define LOC_TYPES X(REG), X(MEM), X(REGIND), X(REGINDOFF), X(MEMIND), X(MEMINDOFF)
+typedef enum {
+#define X(x) x
+	LOC_TYPES
+#undef X
+} loc_t;
+char const *loc_type_names[] = {
+#define X(x) #x
+	LOC_TYPES
+#undef x	
+};
+
+struct loc {
+	loc_t type;
+	union {
+		struct {int r;} reg;
+		struct {int addr;} mem;
+		struct {int r;} regind;
+		struct {int r; int off;} regindoff;
+		struct {int addr;} memind;
+		struct {int addr; int off;} memindoff;
+	};
+};
+
+struct instr {
+	op_t type;
+	union {
+		struct {loc dst; loc src;} cpy;
+		struct {char op; loc dst; loc src1; loc src2;} bop;
+		struct {char op; loc dst; loc src;} uop;
+		struct {int off;} jz, jn, jp, ju;
+		struct {int num;} syscall;
+	};
+};
 
 int skipSpaces(char const* str) {
 	int pos = 0;
@@ -44,22 +91,23 @@ int skipSpaces(char const* str) {
 	return pos;
 }
 
-int printLocation(char const* str) {
+int printLocation(char const* str, loc &res) {
 	int pos = skipSpaces(str);
 	int tmp;
-	int num;
 	switch(str[pos++]) {
 		case 'r':
+			res.type = REG;
 			printf("register ");
-			sscanf(str + pos, "%d%n", &tmp, &num);
-			pos += num;
-			printf("%d", tmp);
+			sscanf(str + pos, "%d%n", &(res.reg.r), &tmp);
+			pos += tmp;
+			printf("%d", res.reg.r);
 			break;
 		case 'm':
+			res.type = MEM;
 			printf("memory at ");
-			sscanf(str + pos, "%x%n", &tmp, &num);
-			pos += num;
-			printf("0x%X", tmp);
+			sscanf(str + pos, "%x%n", &(res.mem.addr), &tmp);
+			pos += tmp;
+			printf("0x%X", res.mem.addr);
 			break;
 		case 'i':
 			puts("Not implemented (i)");
@@ -81,47 +129,50 @@ int printLocation(char const* str) {
 }
 
 //Return number of characters read
-int parsenprint(char const *str) {
+int parsenprint(char const *str, vector<instr> &parsed) {
 	int pos = 0;
 	//Skip past spaces
 	pos += skipSpaces(str);
 	int tmp;
+	instr res;
 	switch(str[pos++]) {
 		case 'c':
+			res.type = CPY;
 			printf("Copy into ");
-			pos += printLocation(str + pos);
+			pos += printLocation(str + pos, res.cpy.dst);
 			printf(" from ");
-			pos += printLocation(str + pos);
+			pos += printLocation(str + pos, res.cpy.src);
 			printf("\n");
 			break;
 		case 'b':
+			res.type = BOP;
 			printf("Binary op: ");
 			pos += skipSpaces(str + pos);
-			tmp = str[pos++];
-			pos += printLocation(str + pos);
+			res.bop.op = str[pos++];
+			pos += printLocation(str + pos, res.bop.dst);
 			printf(" = ");
-			pos += printLocation(str + pos);
-			printf(" %c ", tmp);
-			pos += printLocation(str + pos);
+			pos += printLocation(str + pos, res.bop.src1);
+			printf(" %c ", res.bop.op);
+			pos += printLocation(str + pos, res.bop.src2);
 			printf("\n");
 			break;
 		case 'u':
+			res.type = UOP;
 			printf("Unary op: ");
 			pos += skipSpaces(str + pos);
-			tmp = str[pos++];
-			pos += printLocation(str + pos);
+			res.uop.op = str[pos++];
+			pos += printLocation(str + pos, res.uop.dst);
 			printf(" = ");
-			printf(" %c", tmp);
-			pos += printLocation(str + pos);
+			printf(" %c", res.uop.op);
+			pos += printLocation(str + pos, res.uop.src);
 			printf("\n");
 			break;
-		case 's':{
-			int num;
-			sscanf(str + pos, "%d%n", &num, &tmp);
+		case 's':
+			res.type = SYSCALL;
+			sscanf(str + pos, "%d%n", &(res.syscall.num), &tmp);
 			pos += tmp;
-			printf("Syscall #%d\n", num);
+			printf("Syscall #%d\n", res.syscall.num);
 			break;
-		}
 		case 0:
 			puts("done");
 			return 0;
@@ -129,7 +180,8 @@ int parsenprint(char const *str) {
 			puts("Not implemented");
 			return -1;
 	}
-	
+	parsed.push_back(move(res)); //Right now, the instr type is POD, so moving doesn't do anything.
+	//Actually, I only wrote it because it was easy. I don't actally care about performance here.
 	return pos;
 }
 
@@ -137,10 +189,10 @@ int main() {
 	char const* test = " cr4mBEEFs75b-r3r4r7cr4r5u-r1r1";
 	int pos = 0;
 	int tmp;
-	while((tmp = parsenprint(test+pos)) > 0) {
+	vector<instr> parsed;
+	while((tmp = parsenprint(test+pos, parsed)) > 0) {
 		//printf("tmp = %d\n", tmp);
 		pos += tmp;
 		printf("\n");
 	}
-	cout << "Hello world" << endl;
 }
