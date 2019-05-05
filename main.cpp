8bbfbbe8a06bfb226a01727043f02940e5157673
Marco Merlini <mahkoe@gmail.com>
2019-05-05 17:21:03 -0400

    Started adding scheduling. It's not done yet

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <cstdio> //For sscanf
 #include <utility> //For std::move
+#include <queue> //For priority_queue
 #include "vm.h"
 #include "parse.h"
 
@@ -17,6 +18,37 @@ ostream& operator<< (ostream &o, vector<T> const& v) {
 	return o << "\n}";
 }
 
+#define TASK_STATES X(RUNNING), X(READY), X(WAITING)
+typedef enum {
+#define X(x) x
+	TASK_STATES
+#undef X
+} task_state_t;
+char const *task_state_names[] = {
+#define X(x) #x
+	TASK_STATES
+#undef X
+};
+
+struct task {
+	string name;
+	vector<instr> text;
+	int pos;
+	int regSave[NUM_REGS];
+	
+	task_state_t state;
+	int weight;
+	int vruntime; 
+};
+
+ostream& operator<< (ostream &o, task const& t) {
+	return o << t.name << ": " << task_state_names[t.state] << ", w=" << t.weight << ", vr=" << t.vruntime;
+}
+
+bool operator< (task const& t1, task const& t2) {
+	return t1.vruntime < t2.vruntime;
+}
+
 int main() {
 	char const* fib = 
 		"cr3v20" //Loop count
@@ -27,7 +59,7 @@ int main() {
 		"cm1r1"
 		//Main loop:
 		"b-r3r3v1" //Decrement r3
-		"jn 20" //Quit if loop is finished
+		"jn20" //Quit if loop is finished
 		"b+r5r0r1" //Compute next Fibonacci number
 		"ci2r5" //Store it in memory
 		"b+r2r2v1" //Increment r2
@@ -44,7 +76,7 @@ int main() {
 		"cm33r1"
 		//Main loop:
 		"b-r3r3v1" //Decrement r3
-		"jn 20" //Quit if loop is finished
+		"jn9" //Quit if loop is finished
 		"b+r5r0r1" //Compute next Fibonacci number
 		"ci2r5" //Store it in memory
 		"b+r2r2v1" //Increment r2
@@ -58,7 +90,7 @@ int main() {
 		"cr1v1" //First number
 		//Main loop:
 		"b-r3r3v1" //Decrement r3
-		"jn 20" //Quit if loop is finished
+		"jn20" //Quit if loop is finished
 		"ci2r1" //mem[r2] = r1
 		"b+r1r1v1" //Increment r1
 		"b+r2r2v1" //Increment r2
@@ -73,10 +105,47 @@ int main() {
 	cout << "Lucas program: " << parsed2 << endl;
 	cout << "Iota program: " << parsed3 << endl;
 	vm init;
+	
+	task t1 = {
+		.name = "Fibonacci",
+		.text = parsed1,
+		.pos = 0,
+		.state = READY,
+		.weight = 5,
+		.vruntime = 0
+	};
+	task t2 = {
+		.name = "Lucas",
+		.text = parsed2,
+		.pos = 0,
+		.state = READY,
+		.weight = 4,
+		.vruntime = 0
+	};
+	task t3 = {
+		.name = "Iota",
+		.text = parsed3,
+		.pos = 0,
+		.state = READY,
+		.weight = 1,
+		.vruntime = 0
+	};
+	
+	vector<task> masterlist = {t1, t2, t3};
+	priority_queue<task> pq;
+	//pq.push(t1); pq.push(t2); pq.push(t3);
+	for (auto const& i : masterlist) {
+		cout << i << endl;
+		pq.push(i);
+	}
+	
 	runProg(parsed1, init);
 	cout << init;
 	runProg(parsed2, init);
 	cout << init;
 	runProg(parsed3, init);
 	cout << init;
+	
+	
+	return 0;
 }
